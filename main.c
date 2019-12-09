2fd998134a56e860b9048de5569056e8e20b6153
Dallen Hoyal <dallen.hoyal@gmail.com>
2019-12-09 00:59:32 -0700

    movement and other sprites

--- main.c
+++ main.c
@@ -1,30 +1,72 @@
 #include <gb/gb.h>
 #include <stdio.h>
-#include "FaceSprites.c"
+#include "./sprites/SnakeHead/SnakeHead.c"
+#include "./sprites/SnakeBody/SnakeBody.c"
+
+
+int determineDirection() {
+
+    switch(joypad()) {
+        case J_LEFT:
+            return 0;
+            break;
+        case J_UP:
+            return 1;
+            break;
+        case J_RIGHT:
+            return 2;
+            break;
+        case J_DOWN:
+            return 4;
+            break;
+    }
+}
+
+
 
 void main() {
     UINT8 currentSpriteIndex = 0;
 
-    set_sprite_data(0, 2, Face);
-    set_sprite_tile(0, 0);
-    move_sprite(0, 88, 78);
+    int direction = 0;
+    int xPos = 80;
+    int yPos = 78;
+
+    set_sprite_data(0, 8, SnakeHead);   // first sprite, load 8 sprites, from SnakeHead sprite
+    set_sprite_tile(0, 0);              // first sprite, first sprite memory bank
+    move_sprite(0, xPos, yPos);             // first sprite, 88 right, 78 down
+
+    set_sprite_data(8, 1, SnakeBody);
+    set_sprite_tile(1, 8);
+    move_sprite(1, 88, 78);
+
+
     SHOW_SPRITES;
 
     while(1) {
-        switch(joypad()) {
-            case J_LEFT:
-                scroll_sprite(0, -8, 0);
+        if (joypad()) {
+            direction = determineDirection();
+        }
+
+        switch(direction) {
+            case 0:
+                set_sprite_tile(0, 0);
+                xPos -= 8;
                 break;
-            case J_RIGHT:
-                scroll_sprite(0, 8, 0);
+            case 1:
+                set_sprite_tile(0, 2);
+                yPos -= 8;
                 break;
-            case J_UP:
-                scroll_sprite(0, 0, -8);
+            case 2:
+                set_sprite_tile(0, 4);
+                xPos += 8;
                 break;
-            case J_DOWN:
-                scroll_sprite(0, 0, 8);
+            case 4:
+                set_sprite_tile(0, 6);
+                yPos += 8;
                 break;
         }
-        delay(100);
+
+        move_sprite(0, xPos, yPos);
+        delay(200);
     }
 }
--- sprites/SnakeHead/SnakeHead.h
+++ sprites/SnakeHead/SnakeHead.h
@@ -10,7 +10,7 @@
   Compression          : None.
   Counter              : None.
   Tile size            : 8 x 8
-  Tiles                : 0 to 1
+  Tiles                : 0 to 7
 
   Palette colors       : None.
   SGB Palette          : None.
@@ -24,8 +24,8 @@
 
 
 /* Bank of tiles. */
-#define TileLabelBank 0
+#define SnakeHeadBank 0
 /* Start of tile array. */
-extern unsigned char TileLabel[];
+extern unsigned char SnakeHead[];
 
 /* End of SNAKE_HEAD.H */
